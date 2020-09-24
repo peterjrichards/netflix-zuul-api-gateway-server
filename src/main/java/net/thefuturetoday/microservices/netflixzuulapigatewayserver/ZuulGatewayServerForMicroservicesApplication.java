412bb47363cba559e8fe0a2a78e5312f008c2a67
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 19:57:02 +0100

    Initial Spring Initializr commit

---
+++ src/main/java/net/thefuturetoday/microservices/netflixzuulapigatewayserver/ZuulGatewayServerForMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package net.thefuturetoday.microservices.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayServerForMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServerForMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/thefuturetoday/microservices/netflixzuulapigatewayserver/ZuulGatewayServerForMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thefuturetoday.microservices.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServerForMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
