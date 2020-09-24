7dfb83e9764c546b9af87c3be8cb28e64d61e943
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 20:43:43 +0100

    Added Spring Sleuth

--- src/main/java/net/thefuturetoday/microservices/netflixzuulapigatewayserver/ZuulGatewayServerForMicroservicesApplication.java
+++ src/main/java/net/thefuturetoday/microservices/netflixzuulapigatewayserver/ZuulGatewayServerForMicroservicesApplication.java
@@ -1,9 +1,11 @@
 package net.thefuturetoday.microservices.netflixzuulapigatewayserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,4 +16,10 @@ public class ZuulGatewayServerForMicroservicesApplication {
 		SpringApplication.run(ZuulGatewayServerForMicroservicesApplication.class, args);
 	}
 
+	@Bean
+	//creating a sampler called always sampler
+	public Sampler defaultSampler()
+	{
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
